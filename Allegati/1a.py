cbbeee97dfd9fcdb90939d9cd34c12ba9bc599e9
manuelcaccone <62841081+manuelcaccone@users.noreply.github.com>
2021-03-21 20:02:11 +0100

    update

--- Allegati/1a.py
+++ Allegati/1a.py
@@ -1,26 +1,6 @@
-import numpy as np
-import tensorflow as tf
-from tensorflow_probability import distributions as tfd
-
-tf.enable_eager_execution()
-
-# Set values for the mixture
-alphas = [0.6, 0.3, 0.1]
-means = [30, 60, 120]
-sigmas = [5, 3, 1]
-
-# Use the tf.probability class
-gm = tfd.MixtureSameFamily(
-    mixture_distribution=tfd.Categorical(probs=alphas),
-    components_distribution=tfd.Normal(
-        loc=means,       
-        scale=sigmas))
-
-# Draw 1e5 random samples from the mixture distribution
-prices = gm.sample(sample_shape=(int(1e5)), seed=42)
-
-# Create a normal distribution with empirical mean & std. deviation
-nd_empirical = tfd.Normal(loc=np.mean(prices), scale=np.std(prices))
-
-print(np.mean(prices))
-print(np.std(prices))
+import matplotlib.pyplot as plt
+count, bins, ignored = plt.hist(s, 30, density=True)
+plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *
+               np.exp( - (bins - mu)**2 / (2 * sigma**2) ),
+         linewidth=2, color='r')
+plt.show()
