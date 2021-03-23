e34d71af77dc41161d15f99a3926fc253e7f4335
manuelcaccone <62841081+manuelcaccone@users.noreply.github.com>
2021-03-23 12:36:14 +0100

    update

--- Allegati/2a.R
+++ Allegati/2a.R
@@ -15,3 +15,19 @@ RAA
 # una prima possibilità sta nell'osservare gli sviluppi dei sinistri 
 # per poter osservare la "longevità" di alcune generazioni oppure l'onerosità 
 plot(RAA)
+
+# una rappresentazione più ordinata, tenendo conto anche dell'anno di origine
+# potrebbe essere la seguente
+plot(RAA,lattice=TRUE)
+
+
+# passiamo dalla triangolare dei pagati alla triangolare dei cumulati 
+raa.inc <- cum2incr(RAA)
+
+
+# può capitare che io abbia a disposizione dati da Excel 
+# scarichiamo questo file di esempio 
+# https://github.com/manuelcaccone/CORSO-SIA----Basic-of-R-for-actuaries/blob/main/Allegati/2a.xlsx
+# ora copiamo ed incolliamo da Excel 
+tri <- read.table(file="clipboard", sep="\t", na.strings="NA")
+
