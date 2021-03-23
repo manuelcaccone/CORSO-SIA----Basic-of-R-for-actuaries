b8237c0315c5432494496ddd9601f2ce1bf2da43
manuelcaccone <62841081+manuelcaccone@users.noreply.github.com>
2021-03-23 16:38:00 +0100

    update

--- Allegati/2b.R
+++ Allegati/2b.R
@@ -1,3 +1,15 @@
+## esercitazione PRICING Health 
+
+
+## ATTENZIONE: per poter usare RODBC dovrai usare la versione 32bit di R. Da 
+# RStudio vai su Tools>Global Options> General > R Version > Change/Browse
+# da qui rimanda il "Seleziona cartella" alla cartella C:\Program Files\R\R-3.5.3\bin\i386
+# Chiudi RStudio e Riapri.
+
+# installo il pacchetto se necessario
+install.packages('RODBC',dep=T)
+
+# richiamo il pacchetto
 library(RODBC)
 setwd(readClipboard())
 myChannell<- odbcConnectAccess2007("Database.accdb")
